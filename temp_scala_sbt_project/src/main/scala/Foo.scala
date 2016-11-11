e990d15f93ed4730d89bdf05d27b7bfc3ec34031
Julio CN Souza <juliocnsouzadev@gmail.com>
2016-11-10 22:41:58 -0200

    temp sbt

---
+++ temp_scala_sbt_project/src/main/scala/Foo.scala
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+object Foo {
+
+  /**
+   * @param args the command line arguments
+   */
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+
+}
