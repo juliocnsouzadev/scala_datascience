76ee2d40653e91351742280edecdad709c0e0606
Julio CN Souza <juliocnsouzadev@gmail.com>
2017-01-18 08:48:56 -0200

    for expressions

--- fuctional_programming_principles/fpps-notes/.worksheet/src/week6.Notes.scala
+++ fuctional_programming_principles/fpps-notes/.worksheet/src/week6.Notes.scala
@@ -33,20 +33,33 @@ object Notes {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._;
 
   val pairs = (allNumbers reverse) zip julio;System.out.println("""pairs  : scala.collection.immutable.Vector[(Int, Char)] = """ + $show(pairs ));$skip(16); val res$14 = 
 
-  pairs.unzip;System.out.println("""res14: (scala.collection.immutable.Vector[Int], scala.collection.immutable.Vector[Char]) = """ + $show(res$14));$skip(21); val res$15 = 
+  pairs.unzip;System.out.println("""res14: (scala.collection.immutable.Vector[Int], scala.collection.immutable.Vector[Char]) = """ + $show(res$14));$skip(19); val res$15 = 
+
+  allNumbers sum;System.out.println("""res15: Int = """ + $show(res$15));$skip(23); val res$16 = 
+
+  allNumbers product;System.out.println("""res16: Int = """ + $show(res$16));$skip(19); val res$17 = 
+
+  allNumbers max;System.out.println("""res17: Int = """ + $show(res$17));$skip(19); val res$18 = 
+
+  allNumbers min;System.out.println("""res18: Int = """ + $show(res$18));$skip(44); val res$19 = 
+
+  allNumbers filter (n => n % 2 == 0) sum;System.out.println("""res19: Int = """ + $show(res$19));$skip(44); val res$20 = 
+
+  allNumbers filter (n => n % 2 != 0) sum;System.out.println("""res20: Int = """ + $show(res$20));$skip(72); 
+
+  def isPrime(n: Int): Boolean =
+    (2 until n) forall  (n % _ != 0);System.out.println("""isPrime: (n: Int)Boolean""");$skip(61); 
+
+  //CONBINATORIAL SEARCH AND FOR-EXPRESSIONS
+
+  val n = 7;System.out.println("""n  : Int = """ + $show(n ));$skip(106); val res$21 = 
+  (1 until n) flatMap (i =>
+    (1 until i) map (j => (i, j))) filter(pair => isPrime(pair._1 + pair._2));System.out.println("""res21: scala.collection.immutable.IndexedSeq[(Int, Int)] = """ + $show(res$21));$skip(161); 
+                                                  
+ def scalarProduct(xs: List[Double], ys: List[Double]):Double =
+  (for ((x,y) <- xs zip ys ) yield x * y).sum;System.out.println("""scalarProduct: (xs: List[Double], ys: List[Double])Double""");$skip(44); val res$22 = 
   
-  allNumbers sum;System.out.println("""res15: Int = """ + $show(res$15));$skip(25); val res$16 = 
-  
-  allNumbers product;System.out.println("""res16: Int = """ + $show(res$16));$skip(21); val res$17 = 
-  
-  allNumbers max;System.out.println("""res17: Int = """ + $show(res$17));$skip(21); val res$18 = 
-  
-  allNumbers min;System.out.println("""res18: Int = """ + $show(res$18));$skip(45); val res$19 = 
-  
-  allNumbers filter (n => n % 2 == 0) sum;System.out.println("""res19: Int = """ + $show(res$19));$skip(45); val res$20 = 
-  
-  allNumbers filter (n => n % 2 != 0) sum;System.out.println("""res20: Int = """ + $show(res$20));$skip(80); 
-  
-  def isPrime (n: Int): Boolean =
-   ( 1 to n by 1) forall (x => x % n == 0);System.out.println("""isPrime: (n: Int)Boolean""")}
+  scalarProduct(List(10,20), List(5,15));System.out.println("""res22: Double = """ + $show(res$22))}
+ 
+                                                   
 }
