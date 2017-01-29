f6b64dd9317085da6056b263d1c75e79187f38e3
Julio CN Souza <juliocnsouzadev@gmail.com>
2017-01-29 17:36:46 -0200

    word count

---
+++ spark-with-scala/SparkWithScala/.worksheet/src/br.com.juliocnsouza.spark.scala.spark_basics.MorePreciptatioons.scala
@@ -0,0 +1,5 @@
+package br.com.juliocnsouza.spark.scala.spark_basics
+
+object MorePreciptatioons {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(125); 
+  println("Welcome to the Scala worksheet")}
+}
