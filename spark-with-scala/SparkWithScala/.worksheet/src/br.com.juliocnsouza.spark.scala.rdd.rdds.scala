f12843d9fd78d640b8e81f4cc48cb863eada9a5d
Julio CN Souza <juliocnsouzadev@gmail.com>
2017-02-22 22:56:44 -0300

    update

--- spark-with-scala/SparkWithScala/.worksheet/src/br.com.juliocnsouza.spark.scala.rdd.rdds.scala
+++
@@ -1,5 +0,0 @@
-package br.com.juliocnsouza.spark.scala.rdd
-
-object rdds {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(102); 
-  println("Welcome to the Scala worksheet")}
-}
